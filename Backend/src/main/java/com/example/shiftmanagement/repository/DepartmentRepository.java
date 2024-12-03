27a09bcc7b08d62ff742a2cad16c0ac7d53a6f01
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2024-12-03 19:27:30 +0530

    Delete Backend directory

--- Backend/src/main/java/com/example/shiftmanagement/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shiftmanagement.repository;
-
-import com.example.shiftmanagement.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Optional<Department> findByDepartmentName(String departmentName);  // Updated method name
-}
--- Backend/src/main/java/com/example/shiftmanagement/util/JwtUtil.java
+++
@@ -1,5 +0,0 @@
-package com.example.shiftmanagement.util;
-
-public class JwtUtil {
-
-}
